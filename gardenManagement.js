56809d3c0484e0184e240ba6b1e7a09dedf6d3c1
Abigael Ongwae <abbykerry17@gmail.com>
2025-11-17 19:03:47 +0300

    completed all tasks on garden management assignment

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,25 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80){
+    console.log("Watering on");
+}
+else {
+    console.log ("Watering off");
+}
+
+//checking time of day
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
+//soil moisture while loop
+
+while (soilMoisture < 40){
+    soilMoisture +=5;
+    console.log (soilMoisture);
+}
